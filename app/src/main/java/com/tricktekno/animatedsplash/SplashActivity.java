1ca41250393195c71097f64151733e2b757631c3
optnio00 <anuragpatel.optnio@gmail.com>
2017-05-14 20:18:45 +0530

    Android Animated Splash Screen

---
+++ app/src/main/java/com/tricktekno/animatedsplash/SplashActivity.java
@@ -0,0 +1,58 @@
+package com.tricktekno.animatedsplash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class SplashActivity extends Activity {
+    Thread splashTread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout mainLinearLayout=(LinearLayout) findViewById(R.id.ll_main);
+        mainLinearLayout.clearAnimation();
+        mainLinearLayout.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash_img);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this,SecondActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashActivity.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+
+}
